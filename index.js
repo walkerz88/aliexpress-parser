0c0d73899ee6148544b91fe1eac525323103debf
Alexander <mailwalkerz@yandex.ru>
2020-12-28 11:36:44 +0300

    restart function

--- index.js
+++ index.js
@@ -2,127 +2,152 @@ const scraper = require('./src/modules/scraper');
 const getProductIdByName = require('./src/getProductIdByName');
 const prompts = require('prompts');
 
-(async () => {
-		let searchType = await prompts({
-			type: 'select',
+const initTool = async () => {
+	let searchType = await prompts({
+		type: 'select',
+		name: 'value',
+		message: 'Search product by:',
+		choices: [
+			{ title: 'id', value: 'id' },
+			{ title: 'name', value: 'name' }
+		],
+		initial: 0
+	});
+
+	searchType = searchType.value;
+
+	if (!searchType) {
+		return;
+	}
+
+	if (searchType === 'id') {
+		let productId = await prompts({
+			type: 'number',
 			name: 'value',
-			message: 'Search product by:',
-			choices: [
-				{ title: 'id', value: 'id' },
-				{ title: 'name', value: 'name' }
-			],
-			initial: 0
+			message: 'Product ID:',
+			validate: value => value ? true : 'Please, enter ID of the product.'
 		});
 
-		searchType = searchType.value;
+		productId = productId.value;
 
-		if (!searchType) {
+		if (!productId) {
 			return;
 		}
 
-		if (searchType === 'id') {
-			let productId = await prompts({
-				type: 'number',
-				name: 'value',
-				message: 'Product ID:',
-				validate: value => value ? true : 'Please, enter ID of the product.'
-			});
-	
-			productId = productId.value;
-	
-			if (!productId) {
-				return;
-			}
+		getProductById(productId).then(res => {
+			printResults(res);
+		});
+	} else if (searchType === 'name') {
+		let productName = await prompts({
+			type: 'text',
+			name: 'value',
+			message: 'Product name: ',
+			validate: value => value ? true : 'Please, enter product name.'
+		});
 
-			getProductById(productId).then(res => {
-				printResults(res);
-			});
-		} else if (searchType === 'name') {
-			let productName = await prompts({
-				type: 'text',
-				name: 'value',
-				message: 'Product name: ',
-				validate: value => value ? true : 'Please, enter product name.'
-			});
+		productName = productName.value;
 
-			productName = productName.value;
-
-			if (!productName) {
-				return;
-			}
-
-			getProductIdByName(productName).then(id => {
-				if (id) {
-					console.log(`Product id: ${id}`)
-					getProductById(id).then((res) => {
-						printResults(res);
-					});
-				}
-			});
+		if (!productName) {
+			return;
 		}
 
-		function printResults (product) {
-			const feedback = product.feedback;
-			if (feedback && Array.isArray(feedback) && feedback.length) {
-				let array = feedback.filter(item => item.content);
-				if (array.length) {
-					array = array.map(item => {
-						return {
-							rating: item.rating,
-							content: item.content
-						}
-					});
-
-					let results = {};
-
-					for (let i = 1; i <= 5; i++) {
-						let starsArray = array.filter(item => item.rating === i);
-						results[i] = starsArray;
-					}
-
-					if (Object.keys(results).length) {
-
-						for (key in results) {
-							let reviews = results[key];
-
-							if (reviews && Array.isArray(reviews) && reviews.length) {
-								console.log('');
-								console.log(`Оценка: ${key}`);
-								reviews = reviews.map(item => item.content);
-								console.log(reviews);
-							}
-						}
-					}
-				} else {
-					console.log('');
-					console.log('\x1b[36m%s\x1b[0m', 'No feedbacks received for this product.');
-				}
-
-				printFooter (product.title);
-
-			} else {
-				console.log('');
-				console.log('\x1b[36m%s\x1b[0m', 'No feedbacks received for this product.');
-				printFooter (product.title);
-			}
-		}
-
-		function printFooter (text) {
-			console.log('');
-			console.log('---^-----------');
-			console.log(text);
-			console.log('');
-		}
-
-		function getProductById (id) {
-			return new Promise(resolve => {
-
-				const product = scraper(id);
-				console.log('Fetching product data, please wait...');
-				
-				product.then(res => {
-					resolve(res);
+		getProductIdByName(productName).then(id => {
+			if (id) {
+				console.log(`Product id: ${id}`)
+				getProductById(id).then((res) => {
+					printResults(res);
 				});
-			})
+			}
+		});
+	}
+}
+
+initTool();
+
+function printResults (product) {
+	const feedback = product.feedback;
+	if (feedback && Array.isArray(feedback) && feedback.length) {
+		let array = feedback.filter(item => item.content);
+		if (array.length) {
+			array = array.map(item => {
+				return {
+					rating: item.rating,
+					content: item.content
+				}
+			});
+
+			let results = {};
+
+			for (let i = 1; i <= 5; i++) {
+				let starsArray = array.filter(item => item.rating === i);
+				results[i] = starsArray;
+			}
+
+			if (Object.keys(results).length) {
+
+				for (key in results) {
+					let reviews = results[key];
+
+					if (reviews && Array.isArray(reviews) && reviews.length) {
+						console.log('');
+						console.log(`Оценка: ${key}`);
+						reviews = reviews.map(item => item.content);
+						console.log(reviews);
+					}
+				}
+			}
+		} else {
+			console.log('');
+			console.log('\x1b[36m%s\x1b[0m', 'No feedbacks received for this product.');
 		}
-})();
+
+		printFooter (product.title);
+		finish();
+
+	} else {
+		console.log('');
+		console.log('\x1b[36m%s\x1b[0m', 'No feedbacks received for this product.');
+		printFooter (product.title);
+		finish();
+	}
+}
+
+async function finish () {
+	let finishType = await prompts({
+		type: 'select',
+		name: 'value',
+		message: 'Get another product?',
+		choices: [
+			{ title: 'yes', value: true },
+			{ title: 'no', value: false }
+		],
+		initial: 0
+	});
+
+	finishType = finishType.value;
+
+	if (finishType) {
+		initTool();
+	} else {
+		return;
+	}
+}
+
+function printFooter (text) {
+	console.log('');
+	console.log('---^-----------');
+	console.log(text);
+	console.log('');
+}
+
+function getProductById (id) {
+	return new Promise(resolve => {
+
+		const product = scraper(id);
+		console.log('Fetching product data, please wait...');
+		
+		product.then(res => {
+			resolve(res);
+		});
+	})
+}
