f42379c12e714034f8fc0336054b8b83fbd57f59
Alexander <mailwalkerz@yandex.ru>
2021-01-02 14:53:29 +0300

    README.MD

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     searchByNameResultsLength: 64, // if you search product by name, max title length to show in select
     feedbackMinLength: 25,         // min length of feedback text to be in results
     groupBy: 'language',           // language, rating, null
-    sortLanguage: 'RU',            // language priority in sort
+    sortLanguage: 'RU',            // language priority in sort RU, EN, FR, etc
     feedbackPages: 10,             // number of feedback pages to fetch, do not use large number or you can be banned
     feedbacksLimit: 10,            // limit per page (total limit = feedbacksLimit * feedbackPages)
     translate: false               // translate feedbacks (only english language works for now)
